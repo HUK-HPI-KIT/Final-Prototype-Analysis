f9ba3b204e1d5b23e4875c76a49f49b5ed1cd9b9
paul.mattes <paul.mattes@student.hpi.de>
2022-05-06 22:59:09 +0200

    added first prototype

--- dataset.py
+++ dataset.py
@@ -2,6 +2,7 @@ from pathlib import Path
 from torch.utils.data import Dataset
 import json
 import logging
+import pickle
 
 from utils import check_paths
 
@@ -9,26 +10,54 @@ cache_dir = Path("./cache")
 cache_dataset_file_name = Path("dataset_english.json")
 
 class ProductDataset(Dataset):
-    def __init__(self, data_path, force_rebuild=False):
+    def __init__(self, tokenizer, data_path, force_rebuild=False):
         super().__init__()
         self.data_path = Path(data_path)
         check_paths(self.data_path)
 
+        self.tokenizer = tokenizer
         self.cache_dir = cache_dir
         self.cache_dir.mkdir(parents=True, exist_ok=True)
         self.cache_dataset_path = self.cache_dir / cache_dataset_file_name
 
         if force_rebuild or not self.cache_dataset_path.exists():
             logging.info("Building dataset....")
-            self.build_dataset()
+            self.build_dataset(tokenizer)
         else:
             logging.info(f"loading cached dataset from {str(self.cache_dataset_path.resolve())}")
             self.load_cached_dataset()
 
-    def build_dataset(self):
+    def build_dataset(self, tokenizer):
         with self.data_path.open() as dataset_file:
             dataset = json.load(dataset_file)
+        self.dataset = {}
+        self.dataset["sequences"] = []
+        for recommendation, userAnswers in zip(dataset["userAnswers"], dataset["recommendations"]):
+            model_inputs = tokenizer((list(dataset["products"].values()) + userAnswers), padding="max_length")
+            model_inputs["labels"] = recommendation
+            self.dataset["sequences"].append([model_inputs, recommendation])
+        self.dataset["products"] = dataset["products"]
+        self.save_dataset()
+    
+    def save_dataset(self):
+        with self.cache_dataset_path.open("w") as cache_dataset:
+            pickle.dump(self.dataset, cache_dataset)
 
     def load_cached_dataset(self):
         with self.cache_dataset_path.open() as cached_dataset:
             self.dataset = json.load(cached_dataset)
+    
+    def __getitem__(self, idx):
+        return self.dataset["sequences"][idx]
+
+    def __len__(self):
+        return len(self.dataset["sequences"])
+    
+    def build_data_point(self, userAnswers):
+        return self.tokenizer((list(self.dataset["products"].values()) + userAnswers), padding="max_length")
+
+    def get_ranked_products(self, scores):
+        product_list = list(self.dataset["products"].keys())
+        ranked_product_scores = sorted(list(zip(product_list, scores)), lambda a, b: a[1] > b[1])
+        return [pair[0] for pair in ranked_product_scores]
+    
--- train.py
+++ train.py
@@ -19,3 +19,63 @@
 #     # pretty print last ouput tokens from bot
 #     print("DialoGPT: {}".format(tokenizer.decode(chat_history_ids[:, bot_input_ids.shape[-1]:][0], skip_special_tokens=True)))
 
+import numpy as np
+import argparse
+from typing import Union
+import logging
+import random
+from pathlib import Path
+from transformers import AutoTokenizer, Trainer, TrainingArguments
+from dataset import ProductDataset
+from model import InsurwayRecommender
+
+from utils import set_logging
+
+def main(args: argparse.Namespace) -> None:
+    set_logging(args.log_file, args.log_level, args.log_stdout)
+    tokenizer = AutoTokenizer.from_pretrained("xlm-roberta-base")
+    
+    train_dataset = ProductDataset(tokenizer, args.data_path, args.force_rebuild)
+    if args.mode == "train":
+        model = InsurwayRecommender.from_pretrained(
+            "xlm-roberta-base", config={"num_classes": 3}   
+        )
+        train_args = TrainingArguments("test-trainer", num_train_epochs=4, fp16=True, do_eval=False)
+        trainer = Trainer(model, train_args, train_dataset=train_dataset, tokenizer=tokenizer)
+        trainer.train()
+        model.save_pretrained("trained_models")
+    if args.mode == "inference":
+        model = InsurwayRecommender.from_pretrained("trained_models")
+        user_answers = []
+        while True:
+            user_input = input("User: ")
+            if user_input == "end":
+                break
+            if user_input == "clear":
+                user_answers = []
+            user_answers.append(user_input)
+            model_input = train_dataset.build_data_point(user_answers)
+            prediction = model(model_input)
+            print("Model:\n", "\n".join(train_dataset.get_ranked_products(prediction)))
+    
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser("HUK HPI KIT final prototype analysis tool")
+    
+    parser.add_argument("--log-level", type=str, default="info",
+                     choices=["debug", "info", "warning", "error", "critical"],
+                     help="log level for logging message output")
+    parser.add_argument("--log-file", type=str, default="log.log",
+                     help="output file path for logging. default to stdout")
+    parser.add_argument("--log-stdout", action="store_true", default=False,
+                     help="toggles force logging to stdout. if a log file is specified, logging will be "
+                     "printed to both the log file and stdout")
+    parser.add_argument("--data-path", type=str, required=True,
+                     help="path to dataset json")
+    parser.add_argument("--mode", type=str, required=True,
+                     help="mode")
+    parser.add_argument("--force-rebuild", action="store_true", default=False,
+                     help="forces rebuild of dataset")
+
+    args = parser.parse_args()
+    main(args)
