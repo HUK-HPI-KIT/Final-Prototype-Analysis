281d82c7b8bf3a4a64d74317b971141b790d0695
paul.mattes <paul.mattes@student.hpi.de>
2022-05-12 13:07:57 +0200

    added draft of explanantion generation

--- model_rf.py
+++ model_rf.py
@@ -11,8 +11,32 @@ UNDEFINED_USER_INFORMATION = -1
 class ProductForest(RandomForestClassifier):
     name = ""
     feature_names = []
+    leaf_vote_lookup = []
     
-    def compute_recommendation(self, user_information):
+    def build_leaf_lookup(self):
+        # for getting all leaf nodes of each subtree in order to determine
+        # final recommendation if not enough user information is provided.
+        # this way we don't have to go one random path and our recommendation is more robust
+        for estimator in self.estimators_:
+            tree = estimator.tree_
+            self.leaf_vote_lookup.append(np.zeros((len(tree.node_count), 2), dtype=int))
+            
+            def traverse(node):
+                if tree.feature[node] != _tree.TREE_UNDEFINED:
+                    entry = traverse(tree.children_left[node])
+                    entry += traverse(tree.children_right[node])
+                else:
+                    value = tree.value[node]
+                    if value == 0:
+                        entry = np.array([value, 0], dtype=int)
+                    else:
+                        entry = np.array([0, value], dtype=int)
+                self.leaf_vote_lookup[-1][node] = entry
+                return entry
+
+            traverse(0)
+    
+    def compute_recommendation(self, user_information, recommendation_vote):
         # TODO: maybe already put this in tree traversal for speedup
         # TODO: implement rigid rules for certain products
         for idx, value in enumerate(user_information):
@@ -21,33 +45,36 @@ class ProductForest(RandomForestClassifier):
                 return 0.0
             # TODO: implement more rules. mooooooooooooooore ruuuuuuuuules.
         
-        return float(self.predict_proba(np.expand_dims(user_information, axis=0))[0][1])
+        return float(recommendation_vote[1] / (recommendation_vote[0] + recommendation_vote[1]))
     
     def process_information(self, user_information):
         # TODO: maybe cache estimator states (where they stopped last time) for speedup
         feature_vote = np.zeros((self.n_features_in_,), dtype=int)
-        for estimator in self.estimators_:
+        recommendation_vote = np.array([1, 0], dtype=int)
+        for idx, estimator in enumerate(self.estimators_):
             tree = estimator.tree_
             
-            def traverse(node, depth):
-                if tree.feature[node] != _tree.TREE_UNDEFINED:
+            def traverse(node):
+                if tree.feature[node] != _tree.TREE_UNDEFINED:  # is split node
                     feature_idx = tree.feature[node]
                     if user_information[feature_idx] == UNDEFINED_USER_INFORMATION:
                         feature_vote[feature_idx] += 1
-                        logging.debug(f"found undefined feature at depth {depth}")
-                        return
+                        # if we reach a split where we don't have enough information we 
+                        # return the number of leafs below this node that vote for 0 and the number of leafs voting 1
+                        return self.leaf_vote_lookup[idx][node]
                     threshold = tree.threshold[node]
                     if user_information[feature_idx] <= threshold:
-                        traverse(tree.children_left[node], depth + 1)
+                        traverse(tree.children_left[node])
                     else:
-                        traverse(tree.children_right[node], depth + 1)
-
-            traverse(0, 1)
-        return feature_vote, self.compute_recommendation(user_information)
+                        traverse(tree.children_right[node])
+                else:  # is leaf node
+                    return self.leaf_vote_lookup[idx][node]
+                    
+            recommendation_vote += traverse(0)
+        return feature_vote, self.compute_recommendation(recommendation_vote)
 
 class ProductRecommender():
     cache_dir = "cache/model"
-    n_features = 12
     
     def __init__(self, dataset, train=False):
         self.model_store_path = Path(self.cache_dir) / "rf_model.joblib"
